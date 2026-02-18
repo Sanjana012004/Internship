0dbe4bbac2e844796dcbc3fb8b2b5155f4ab93ee
Sanjana N <153722639+Sanjana012004@users.noreply.github.com>
2026-02-18 18:02:44 +0530

    Innitial Commit

---
+++ L10/test.js
@@ -0,0 +1,28 @@
+//Uing cosole
+console.log("Hello World!");
+//Using alert
+alert("Hello World!");
+
+//Concatenation
+var num1=5;
+var num2=2;
+alert(num1+num2);
+
+//String and var Concatenation
+var num1=5;
+var num2=2;
+var total= num1+num2;
+alert("The sum is "+total);
+
+//Using confirm
+confirm("Would you like to proceed to payment");
+
+//storing confirm and displaying it
+var cofirmPayment = confirm("Would you like to proceed to payment");
+alert(cofirmPayment);
+
+//Using prompt
+var question = prompt("Are you ready for a brand new day of possibilities?");
+alert(question);
+console.log(question);
+
